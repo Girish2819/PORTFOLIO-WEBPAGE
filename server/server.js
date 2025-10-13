dc3052aa3fd8297e0bc9a304b96074d503fd126b
Girish Ranjan <girishranjan71@gmail.com>
2025-10-14 03:02:04 +0530

    Ready for deployment

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import cors from 'cors';    
 import mongoose from 'mongoose';
@@ -7,26 +8,31 @@ import blogRoutes from './routes/blogRoutes.js';
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 
-//routes
+// API routes
 app.use('/api/contacts', contactRoutes);
 app.use('/api/blogs', blogRoutes);
 
-//mongodb connection
+// MongoDB connection
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.log("mongoDB connection error:",err));
+    .catch((err) => console.log("MongoDB connection error:", err));
 
-    
-app.get('/', (req, res)=>{
-    res.send('server is running...');
+// Serve frontend static files
+app.use(express.static(path.join(__dirname, 'frontend/dist'))); // Vite
+
+// Serve index.html for all other routes
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
 });
 
+// Start server
 const port = process.env.PORT || 5000;
-app.listen(port, ()=>{
-    console.log(`server is running on port ${port}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
